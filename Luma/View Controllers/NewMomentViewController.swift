5d9fd9e28109f66f7cafd923a7f534e3f7e61007
Eddie Chen <Eddie@Chun-Weis-MacBook-Pro.local>
2016-06-07 10:03:30 -0700

    tool bar in new moment vvc

--- Luma/View Controllers/NewMomentViewController.swift
+++ Luma/View Controllers/NewMomentViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class NewMomentViewController: UIViewController {
 
+    var toolBarBottom:UIToolbar!
     var streamSelectionButton:UIButton!
+    var momentContentTableView:UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,15 +22,64 @@ class NewMomentViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create", style: .Done, target: self, action: #selector(NewMomentViewController.createButtonTapped))
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: #selector(NewMomentViewController.cancelButtonTapped))
         
+        
+        toolBarBottom = UIToolbar(frame: CGRectZero)
+        toolBarBottom.translatesAutoresizingMaskIntoConstraints = false
         let addMediaButton = UIBarButtonItem(image: UIImage(named: "addMediaButtonIcon"), style: .Plain, target: self, action: #selector(NewMomentViewController.addMediaButtonTapped))
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
         let addUnlockButton = UIBarButtonItem(image: UIImage(named: "addUnlockSettingsIcon"), style: .Plain, target: self, action: #selector(NewMomentViewController.addUnlockSettingsButtonTapped))
-        navigationController?.toolbar.setItems([addMediaButton, flexSpace, addUnlockButton], animated: false)
+        toolBarBottom.tintColor = Colors.primary
+        toolBarBottom.items = [addMediaButton, flexSpace, addUnlockButton]
+        self.view.addSubview(toolBarBottom)
         
-        navigationController?.toolbarHidden = false
+        let toolBarBottomPinConstraint = NSLayoutConstraint(item: toolBarBottom, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self.bottomLayoutGuide, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0)
+        let toolBarBottomHeightConstraint = NSLayoutConstraint(item: toolBarBottom, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 44)
+        let toolBarBottomWidthConstraint = NSLayoutConstraint(item: toolBarBottom, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Width, multiplier: 1, constant: 0)
+        
+        self.view.addConstraint(toolBarBottomPinConstraint)
+        self.view.addConstraint(toolBarBottomHeightConstraint)
+        self.view.addConstraint(toolBarBottomWidthConstraint)
+        
+        toolBarBottom.removeFromSuperview()
 
         view.backgroundColor = Colors.white
         
+        
+        streamSelectionButton = UIButton(type: .Custom)
+        streamSelectionButton.frame = CGRectZero
+        streamSelectionButton.translatesAutoresizingMaskIntoConstraints = false
+        streamSelectionButton.backgroundColor = Colors.primary
+        view.addSubview(streamSelectionButton)
+        
+        let momentContentTVC = UITableViewController()
+        addChildViewController(momentContentTVC)
+        
+        momentContentTVC.tableView = UITableView(frame: CGRectZero)
+        momentContentTVC.tableView.translatesAutoresizingMaskIntoConstraints = false
+        momentContentTableView = momentContentTVC.tableView
+        momentContentTableView.contentInset.top = 64
+        momentContentTableView.contentInset.bottom = 44
+        view.addSubview(momentContentTableView)
+        
+        let viewsDictionary = ["momentContentTableView":momentContentTableView, "topLayoutGuide":topLayoutGuide, "bottomLayoutGuide":bottomLayoutGuide, "streamSelectionButton":streamSelectionButton]
+        let metricsDictionary = ["sidePadding":15]
+        
+        let tableViewHConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[momentContentTableView]|", options: NSLayoutFormatOptions(rawValue:0), metrics: metricsDictionary, views: viewsDictionary as! [String : AnyObject])
+        let tableViewVConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[topLayoutGuide][momentContentTableView][bottomLayoutGuide]", options: NSLayoutFormatOptions(rawValue:0), metrics: metricsDictionary, views: viewsDictionary as! [String : AnyObject])
+        
+        view.addConstraints(tableViewHConstraints)
+        view.addConstraints(tableViewVConstraints)
+        
+        
+    }
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+
+    
+    override var inputAccessoryView: UIView?{
+        return toolBarBottom
     }
 
     override func didReceiveMemoryWarning() {
